8577b1a29bf04c9dda71ad8a49a6608bf32e3b86
Toney <74755044+Apron2050@users.noreply.github.com>
2021-08-11 16:19:10 +0800

    Create use_services.sh

---
+++ use_services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+http http://m1-alice.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
+http http://m1-alice-currency.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
+http http://m1-alice-oil.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
+http http://m1-alice-news.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
+http http://m1-alice-mobile.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
+http http://m1-alice-weather.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
+http http://m1-alice-city.apron.network:8080/v1/5DcpvLnDPQJyon3EH4kre1WwW2BmdeSgDnEw4zwNEordNxeP/
